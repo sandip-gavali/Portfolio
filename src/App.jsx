fc05cdfbfa29c36c85fe3b4f88d453dddc5815b8
Sandip Gavali <gavalisandip201.com>
2026-03-12 13:37:19 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Services from './Components/Services/Services'
-import MyWork from './Components/Mywork/Mywork'
+import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
